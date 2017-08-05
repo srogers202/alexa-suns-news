093100eb5c4eb6e81ca2b0d4411ad4260aced185
Seth Rogers <sethhrogers@gmail.com>
2017-08-04 21:49:45 -0700

    .gitignore

--- node_modules/gulp-awslambda/example/index.js
+++
@@ -1,8 +0,0 @@
-console.log('Loading function');
-
-exports.handler = (event, context, callback) => {
-	console.log('value1 =', event.key1);
-	console.log('value2 =', event.key2);
-	console.log('value3 =', event.key3);
-	callback(null, 'Hello World from gulp-awslambda!');
-};
